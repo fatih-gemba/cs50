db352a0935ce342828d5588a9123a24394aebc87
tataroglu <fatih.gemba@gmail.com>
2023-03-15 15:33:16 +0300

    dfd

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World!\n");
+
+    string first = get_string("What is your firs name?");
+    string last = get_string("What is your last name?");
+    printf("hello, %s %s\n", rist, last);
+}
