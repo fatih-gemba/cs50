f772fc016c4d0606527e74d604c4f06e2d829af1
tataroglu <fatih.gemba@gmail.com>
2023-03-15 16:31:51 +0300

    bla bla

--- hello.c
+++ hello.c
@@ -6,5 +6,5 @@ int main(void) {
 
     string first = get_string("What is your firs name?");
     string last = get_string("What is your last name?");
-    printf("hello, %s %s\n", rist, last);
+    printf("hello, %s %s\n", first, last);
 }
